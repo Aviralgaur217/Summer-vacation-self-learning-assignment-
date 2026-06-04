b0015d921988f848dc1c0c695de1a5df8bfc7611
Aviralgaur217 <aviralgaur217@gmail.com>
2026-06-04 14:02:59 +0530

    Add files via upload

---
+++ program4.c
@@ -0,0 +1,28 @@
+// check weather a number  is palindrome
+
+#include <stdio.h>
+int main()
+{
+    int n, reversed = 0, remainder, original;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    original = n;
+
+    // reversed integer is stored in variable
+    while (n != 0)
+    {
+        remainder = n % 10;
+        reversed = reversed * 10 + remainder;
+        n /= 10;
+    }
+
+    // palindrome if original and reversed are equal
+    if (original == reversed)
+        printf("%d is a palindrome.", original);
+    else
+        printf("%d is not a palindrome.", original);
+
+    return 0;
+}
