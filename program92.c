8cb529251a522ebc6830171b5ef6e0f3130f260d
Aviralgaur217 <aviralgaur217@gmail.com>
2026-07-18 15:42:49 +0530

    Add files via upload

---
+++ program92.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int i, j, count, max = 0;
+    char maxChar;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    for (i = 0; str[i] != '\0' && str[i] != '\n'; i++) {
+        count = 1;
+
+        if (str[i] == '*')
+            continue;
+
+        for (j = i + 1; str[j] != '\0' && str[j] != '\n'; j++) {
+            if (str[i] == str[j]) {
+                count++;
+                str[j] = '*';   // Mark duplicate
+            }
+        }
+
+        if (count > max) {
+            max = count;
+            maxChar = str[i];
+        }
+    }
+
+    printf("Maximum occurring character = %c\n", maxChar);
+    printf("Frequency = %d\n", max);
+
+    return 0;
+}
